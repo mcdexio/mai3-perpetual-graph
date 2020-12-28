315c24f3635adf644d9184d7e0a5265ae5ba2cc0
renpu-mcarlo <renpu@mcarlo.com>
2020-12-28 20:32:13 +0800

    fix tradesuccess

--- src/broker.ts
+++ src/broker.ts
@@ -2,16 +2,18 @@ import {
     TradeSuccess as TradeSuccessEvent,
 } from '../generated/Broker/Broker'
 
-import { MatchOrder, Perpetual } from '../generated/schema'
+import { MatchOrder, LiquidityPool } from '../generated/schema'
 
 import {
     fetchUser,
     BI_18,
     convertToDecimal,
+    fetchPerpetual
 } from './utils'
 
 export function handleTradeSuccess(event: TradeSuccessEvent): void {
-    let perp = Perpetual.load(event.params.order.perpetual.toHexString())
+    let liquidityPool = LiquidityPool.load(event.params.order.liquidityPool.toHexString())
+    let perp = fetchPerpetual(liquidityPool as LiquidityPool, event.params.order.perpetualIndex)
     let trader = fetchUser(event.params.order.trader)
     let transactionHash = event.transaction.hash.toHexString()
     let order = new MatchOrder(
