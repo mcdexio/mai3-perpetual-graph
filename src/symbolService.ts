cf6efe2b58602eb0196270602f456cfaf0c239c7
renpu-mcarlo <renpu@mcarlo.com>
2021-01-15 19:10:12 +0800

    update symbol

--- src/symbolService.ts
+++ src/symbolService.ts
@@ -13,8 +13,9 @@ export function handleAllocateSymbol(event: AllocateSymbolEvent): void {
     let liquidityPool = LiquidityPool.load(event.params.liquidityPool.toHexString())
     let perp = fetchPerpetual(liquidityPool as LiquidityPool, event.params.perpetualIndex)
     let symbol = event.params.symbol.toString()
-    if (symbol.length < 5) {
-        for (let index = 0; index < 5-symbol.length; index++) {
+    let symbolLen = symbol.length
+    if (symbolLen < 5) {
+        for (let index = 0; index < 5-symbolLen; index++) {
             symbol = "0".concat(symbol)
         }
     }
