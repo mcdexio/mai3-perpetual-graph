8f57ee0c7a97af69fad4773ff60995e8d222d278
renpu-mcarlo <renpu@mcarlo.com>
2021-02-07 10:17:51 +0800

    update block handler

---
+++ src/const.ts
@@ -0,0 +1,13 @@
+// Code generated - DO NOT EDIT.
+
+// Notice lower case
+export const FACTORY_ADDRESS = '0x01c4edb6a57ec75ea1b71b1964b5768348c30e54'
+export const READER_ADDRESS = '0x3563cb8eDEB55f12861a3c194143874b05b1aB9e'
+
+
+// oracle address for get price
+export const ETH_ORACLE = '0x2dcca2b995651158fe129ddd23d658410cea8254'
+
+
+// blocknumber for blockHandler
+export const HANDLER_BLOCK = 1008525
