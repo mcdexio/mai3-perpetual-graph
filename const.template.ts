8b7e4911dc592b005a4f67f43dec556c51ed0a42
renpu-mcarlo <renpu@mcarlo.com>
2021-03-10 17:09:10 +0800

    update for reader contract update

--- const.template.ts
+++ const.template.ts
@@ -1,4 +1,6 @@
 export const READER_ADDRESS = '{{reader_address}}'
+export const OLD_READER_ADDRESS = '{{old_reader_address}}'
+export const NEW_READER_BLOCK = {{new_reader_block}}
 
 // oracle address for get price
 export const ETH_ORACLE = '{{eth_oracle}}'
--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,8 @@
 // Code generated - DO NOT EDIT.
 
 export const READER_ADDRESS = '0x3563cb8eDEB55f12861a3c194143874b05b1aB9e'
+export const OLD_READER_ADDRESS = '0x1605275a33b2ab2f00dc96e75190b3fe49272401'
+export const NEW_READER_BLOCK = 23847386
 
 
 // oracle address for get price
