0adb4c5ca09d3e4463cb50af0adc1dd49770e884
renpu-mcarlo <renpu@mcarlo.com>
2021-04-07 18:11:23 +0800

    update

--- src/factory.ts
+++ src/factory.ts
@@ -2,7 +2,7 @@ import { TypedMap, BigInt, BigDecimal, ethereum, log, Address } from "@graphprot
 
 import { Factory, HandledBlock, LiquidityPool, Perpetual, PriceBucket, PriceMinData, Price15MinData, PriceHourData, PriceDayData, PriceSevenDayData, ShareToken, Governor } from '../generated/schema'
 
-import { CreateLiquidityPool, SetVaultFeeRate, Factory as FactoryContract } from '../generated/Factory/Factory'
+import { CreateLiquidityPool, CreateLiquidityPool1, SetVaultFeeRate, Factory as FactoryContract } from '../generated/Factory/Factory'
 import { Oracle as OracleContract } from '../generated/Factory/Oracle'
 import { Reader as ReaderContract } from '../generated/Factory/Reader'
 import { ReaderV004 as ReaderV004Contract } from '../generated/Factory/ReaderV004'
@@ -74,6 +74,99 @@ export function handleSetVaultFeeRate(event: SetVaultFeeRate): void {
 }
 
 export function handleCreateLiquidityPool(event: CreateLiquidityPool): void {
+    let factory = Factory.load(FACTORY)
+    if (factory === null) {
+        factory = new Factory(FACTORY)
+        factory.liquidityPoolCount = ZERO_BI
+        factory.perpetualCount = ZERO_BI
+        factory.totalVolumeUSD = ZERO_BD
+        factory.totalValueLockedUSD = ZERO_BD
+        factory.totalVaultFeeUSD = ZERO_BD
+        factory.vaultFeeRate = ZERO_BD
+        factory.daoAssetUSD = ZERO_BD
+        let contract = FactoryContract.bind(event.address)
+        let result = contract.try_getVaultFeeRate()
+        if (!result.reverted) {
+            factory.vaultFeeRate = convertToDecimal(result.value, BI_18)
+        }
+        factory.vault = ''
+        let vaultResult = contract.try_getVault()
+        if (!vaultResult.reverted) {
+            factory.vault = vaultResult.value.toHexString()
+        }
+        factory.txCount = ZERO_BI
+        factory.latestBlock = ZERO_BI
+        factory.liquidityPools = []
+        factory.perpetuals = []
+        factory.collaterals = []
+
+        // create price bucket for save eth price
+        let bucket = new PriceBucket('1')
+        bucket.ethPrice = ZERO_BD
+        bucket.timestamp = event.block.timestamp.toI32()  / 3600 * 3600
+        bucket.minTimestamp = event.block.timestamp.toI32()  / 60 * 60
+        bucket.tenMinTimestamp = event.block.timestamp.toI32()  / 600 * 600
+        bucket.save()
+    }
+    factory.liquidityPoolCount = factory.liquidityPoolCount.plus(ONE_BI)
+    let liquidityPools = factory.liquidityPools
+    liquidityPools.push(event.params.liquidityPool.toHexString())
+    factory.liquidityPools = liquidityPools
+    let collateral = event.params.collateral.toHexString()
+    let collaterals = factory.collaterals
+    if (!isCollateralAdded(collaterals as string[], collateral)) {
+        collaterals.push(collateral)
+        factory.collaterals = collaterals
+    }
+    factory.save()
+
+    let liquidityPool = new LiquidityPool(event.params.liquidityPool.toHexString())
+    liquidityPool.voteAddress = event.params.governor.toHexString()
+    liquidityPool.shareAddress = event.params.shareToken.toHexString()
+    liquidityPool.operatorAddress = event.params.operator.toHexString()
+    liquidityPool.operatorExpiration = event.block.timestamp + OPERATOR_EXP
+    liquidityPool.factory = factory.id
+    liquidityPool.collateralAddress = collateral
+    liquidityPool.collateralName = fetchCollateralSymbol(event.params.collateral)
+    liquidityPool.collateralDecimals = event.params.collateralDecimals
+    liquidityPool.vaultFee = ZERO_BD
+    liquidityPool.poolMargin = ZERO_BD
+    liquidityPool.poolMarginUSD = ZERO_BD
+    liquidityPool.liquidityProviderCount = ZERO_BI
+    liquidityPool.createdAtTimestamp = event.block.timestamp
+    liquidityPool.createdAtBlockNumber = event.block.number
+    liquidityPool.isRun = false
+    liquidityPool.liquidityHisCount = ZERO_BI
+    liquidityPool.perpetualIDs = []
+
+    // create share token
+    let shareToken = new ShareToken(event.params.shareToken.toHexString())
+    shareToken.liquidityPool = liquidityPool.id
+    shareToken.totalSupply = ZERO_BD
+    liquidityPool.shareToken = shareToken.id
+
+    // create vote
+    let governor = new Governor(event.params.governor.toHexString())
+    governor.liquidityPool = liquidityPool.id
+    governor.totalVotes = ZERO_BD
+    governor.totalReward = ZERO_BD
+    governor.rewardRate = ZERO_BD
+    governor.periodFinish = ZERO_BI
+    governor.proposalCount = ZERO_BI
+    liquidityPool.governor = governor.id 
+
+    shareToken.save()
+    governor.save()
+    liquidityPool.save()
+
+    // create the tracked contract based on the template
+    LiquidityPoolTemplate.create(event.params.liquidityPool)
+    ShareTokenTemplate.create(event.params.shareToken)
+    GovernorTemplate.create(event.params.governor)
+}
+
+// for old created pools, will be deleted 
+export function handleCreateLiquidityPool1(event: CreateLiquidityPool1): void {
     let factory = Factory.load(FACTORY)
     if (factory === null) {
         factory = new Factory(FACTORY)
