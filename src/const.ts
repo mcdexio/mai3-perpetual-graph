0dca40afe1f862b0aa000344a72caa6ec45c5296
renpu-mcarlo <renpu@mcarlo.com>
2021-05-21 15:48:29 +0800

    1.update abi 2. delete compatible code for create pool

--- src/const.ts
+++ src/const.ts
@@ -1,38 +1,21 @@
 // Code generated - DO NOT EDIT.
-import { TypedMap } from '@graphprotocol/graph-ts'
-
 export const READER_ADDRESS = '0x3563cb8eDEB55f12861a3c194143874b05b1aB9e'
-export const READER_V4_ADDRESS = '0x1605275a33b2ab2f00dc96e75190b3fe49272401'
-export const READER_V5_ADDRESS = '0x1605275a33b2ab2f00dc96e75190b3fe49272401'
 export const DAO_VAULT_ADDRESS = '0x1605275a33b2ab2f00dc96e75190b3fe49272401'
 
-export const READER_V4_BLOCK = 23847386
-export const READER_V5_BLOCK = 23847386
 
 // blocknumber for blockHandler
 export const HANDLER_BLOCK = 1008525
 
-// Notice lower case
-export let ETHTokens:string[] = [
-    "0xdea04ead9bce0ba129120c137117504f6dfaf78f",
-    "0x1520d5561dfb209c6df5149cb6146f6b18d7ad2a",
-    "0x726e650f0bdf5bd57b4a3e23f81973d3c225a94c"
+// Notice lower case in config
+// added ["USDT", "USDC", "DAI"]
+export let USDTokens:string[] = [
+    "0x4b442fb2b62bacce41c202fb244b1d0ca4c7bf8f",
 ]
 
 // !!!!!!!!!!!!!!  Notice Lower Case  !!!!!!!!!!!!!!
-// oracle map (token, oracle)
-export let OracleMap = new TypedMap<string, string>()
-OracleMap.set('0xfa53fd78b5176b4d772194511cc16c02c7f183f9', '0xbb05666820137b3b1344fe6802830515c015dd4f')
-  
-// Notice lower case
-// added ["USDT", "USDC", "DAI"]
-export let USDTokens:string[] = [
-    "0x8b2c4fa78fba24e4cbb4b0ca7b06a29130317093",
-]
-
 // TokenList: tokens need to get price
 export let TokenList:string[] = [
-    "0xfa53fd78b5176b4d772194511cc16c02c7f183f9",
+    "0xfa53fd78b5176b4d77219326841cc16c02c7f183f9",
 ]
 // OracleList: oracles of each token upper, Notice: index must same with its token
 export let OracleList:string[] = [
