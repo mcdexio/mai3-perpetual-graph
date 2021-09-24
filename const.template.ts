8ed04e30b530fab4991dea46e05d3872d44b3aca
renpu-mcarlo <renpu@mcarlo.com>
2021-09-24 11:11:25 +0800

    update template

--- const.template.ts
+++ const.template.ts
@@ -1,9 +1,6 @@
 import {TypedMap} from '@graphprotocol/graph-ts'
 
 export const READER_ADDRESS = '{{reader_address}}'
-export const DAO_POOL = '{{dao_pool}}'
-export const DAO_POOL_NAME = '{{dao_pool_name}}'
-
 
 // Notice lower case in config
 // added ["USDT", "BUSD", "USDT"]
